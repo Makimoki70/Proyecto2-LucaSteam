a9d6ec5030a8d68c1f54fd226a00ae20758a5657
Joel Pascual <joeltrabajo92@gmail.com>
2022-11-22 10:55:55 +0100

    "Subida de prueba"

---
+++ Spring-LucaSteam-Grupo1/Spring-LucaSteam-Grupo1/src/main/java/com/proyecto/spring/service/GameServiceImpl.java
@@ -0,0 +1,26 @@
+package com.proyecto.spring.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.proyecto.spring.model.Game;
+import com.proyecto.spring.repository.GameDAO;
+
+@Service("gameService")
+public class GameServiceImpl implements GameService {
+	
+	@Autowired
+	private GameDAO data;
+
+	@Override
+	public void gameAdd(Game game) {
+		// TODO Auto-generated method stub
+		
+		data.save(game);
+		
+		
+	}
+	
+	
+
+}
