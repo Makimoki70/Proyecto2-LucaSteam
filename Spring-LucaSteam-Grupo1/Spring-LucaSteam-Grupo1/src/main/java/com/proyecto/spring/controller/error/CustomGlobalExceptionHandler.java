2ec841b35e20047a4986f791ea14c463d02ecc0f
Alberto <makimoki69@gmail.com>
2022-11-24 10:05:43 +0100

    Añadida etiqueta @ControllerAdvice. Siguen faltando cosas

--- Spring-LucaSteam-Grupo1/Spring-LucaSteam-Grupo1/src/main/java/com/proyecto/spring/controller/error/CustomGlobalExceptionHandler.java
+++ Spring-LucaSteam-Grupo1/Spring-LucaSteam-Grupo1/src/main/java/com/proyecto/spring/controller/error/CustomGlobalExceptionHandler.java
@@ -14,20 +14,20 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@ControllerAdvice
 public class CustomGlobalExceptionHandler  extends ResponseEntityExceptionHandler{
 
 	
 	
 	@ExceptionHandler(GameNotFoundException.class)
 	public void springHandleNotFound(HttpServletResponse response) throws IOException{
-		
 		logger.info("----- Excepción de estudiante no encontrado()");		
 		response.sendError(HttpStatus.NOT_FOUND.value());		
-		
 	}
 	
 	@ExceptionHandler(ConstraintViolationException.class)
